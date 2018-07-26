ea7d0f95cc091ed892ae5a8191de07f951d64036
Alan Gutierrez <alan@prettyrobots.com>
2018-07-25 23:58:54 -0500

    Rename `assert` to `okay`.

--- t/redux.t.js
+++ t/redux.t.js
@@ -1,6 +1,6 @@
 require('proof')(16, prove)
 
-function prove (assert) {
+function prove (okay) {
     var operation = require('../redux')
     var object = {
         method: function (one, two, three, four) {
@@ -12,35 +12,35 @@ function prove (assert) {
     }, {
         arity: 3, properties: { vargs: [ 1, 2, 3 ] }
     })
-    assert(f.length, 3, 'set arity named')
-    assert(f.vargs, [ 1, 2, 3 ], 'properties')
-    assert(f(1, 2, 3), 6, 'fixed arity named')
+    okay(f.length, 3, 'set arity named')
+    okay(f.vargs, [ 1, 2, 3 ], 'properties')
+    okay(f(1, 2, 3), 6, 'fixed arity named')
     f = operation({
         object: object, method: object.method
     }, {
         arity: 3
     })
-    assert(f.length, 3, 'set arity method')
-    assert(f(1, 2, 3), 6, 'fixed arity method')
+    okay(f.length, 3, 'set arity method')
+    okay(f(1, 2, 3), 6, 'fixed arity method')
     f = operation({ object: object, method: 'method' })
-    assert(f.length, 4, 'variadic arity named')
-    assert(f(1, 2, 3), 6, 'variadic named')
+    okay(f.length, 4, 'variadic arity named')
+    okay(f(1, 2, 3), 6, 'variadic named')
     f = operation({ object: object, method: object.method })
-    assert(f.length, 4, 'variadic arity method')
-    assert(f(1, 2, 3), 6, 'variadic method')
+    okay(f.length, 4, 'variadic arity method')
+    okay(f(1, 2, 3), 6, 'variadic method')
     f = operation(object.method)
-    assert(f.length, 4, 'variadic arity function')
-    assert(f(1, 2, 3), 6, 'variadic function')
+    okay(f.length, 4, 'variadic arity function')
+    okay(f(1, 2, 3), 6, 'variadic function')
     f = operation('method', { arity: 3, object: object })
-    assert(f.length, 3, 'fixed arity default object')
-    assert(f(1, 2, 3), 6, 'default object')
+    okay(f.length, 3, 'fixed arity default object')
+    okay(f(1, 2, 3), 6, 'default object')
     f = operation({ object: object, method: 'missing' })
-    assert(f.length, 0, 'arity missing method')
+    okay(f.length, 0, 'arity missing method')
     object.missing = object.method
-    assert(f(1, 2, 3), 6, 'missing method assigned')
+    okay(f(1, 2, 3), 6, 'missing method assigned')
     try {
         operation({ method: object.method }, 4)
     } catch (error) {
-        assert(error.message, 'arity set through options array', 'arity as argument')
+        okay(error.message, 'arity set through options array', 'arity as argument')
     }
 }
--- t/variadic.t.js
+++ t/variadic.t.js
@@ -1,6 +1,6 @@
 require('proof')(22, prove)
 
-function prove (assert) {
+function prove (okay) {
     var Operation = require('..')
     var object = {
         method: function (one, two, three, four) {
@@ -8,46 +8,46 @@ function prove (assert) {
         }
     }
     var f = Operation([ object, 'method' ], { arity: 3, properties: { vargs: [ 1, 2, 3 ] } })
-    assert(f.vargs, [ 1, 2, 3 ], 'properties')
-    assert(f.length, 3, 'specified arity named arity')
-    assert(f(1, 2, 3), 6, 'specified arity named')
+    okay(f.vargs, [ 1, 2, 3 ], 'properties')
+    okay(f.length, 3, 'specified arity named arity')
+    okay(f(1, 2, 3), 6, 'specified arity named')
     var f = Operation([ object, 'method', [ 1, 2, 3 ] ], { vargs: true })
-    assert(f.vargs, [ 1, 2, 3 ], 'vargs')
-    assert(f.length, 4, 'specified arity named arity')
-    assert(f(1, 2, 3), 6, 'specified arity named')
+    okay(f.vargs, [ 1, 2, 3 ], 'vargs')
+    okay(f.length, 4, 'specified arity named arity')
+    okay(f(1, 2, 3), 6, 'specified arity named')
     var f = Operation([ object, 'method' ], { vargs: true })
-    assert(f.vargs, [], 'empty vargs')
+    okay(f.vargs, [], 'empty vargs')
     f = Operation([ object, object.method ], { arity: 3 })
-    assert(f.length, 3, 'specified arity method arity')
-    assert(f(1, 2, 3), 6, 'specified arity method')
+    okay(f.length, 3, 'specified arity method arity')
+    okay(f(1, 2, 3), 6, 'specified arity method')
     f = Operation([ object, 'method' ])
-    assert(f.length, 4, 'variadic named arity')
-    assert(f(1, 2, 3), 6, 'variadic named')
+    okay(f.length, 4, 'variadic named arity')
+    okay(f(1, 2, 3), 6, 'variadic named')
     f = Operation([ object, object.method ])
-    assert(f.length, 4, 'variadic method arity')
-    assert(f(1, 2, 3), 6, 'variadic method')
+    okay(f.length, 4, 'variadic method arity')
+    okay(f(1, 2, 3), 6, 'variadic method')
     f = Operation([ object.method ])
-    assert(f.length, 4, 'variadic function arity')
-    assert(f(1, 2, 3), 6, 'variadic function')
+    okay(f.length, 4, 'variadic function arity')
+    okay(f(1, 2, 3), 6, 'variadic function')
     f = Operation([ 'method' ], { arity: 3, object: object })
-    assert(f.length, 3, 'default object arity')
-    assert(f(1, 2, 3), 6, 'default object')
+    okay(f.length, 3, 'default object arity')
+    okay(f(1, 2, 3), 6, 'default object')
     f = Operation([ [ object, 'method' ] ], { arity: 3, object: object })
-    assert(f.length, 3, 'array assignment arity')
-    assert(f(1, 2, 3), 6, 'array assignment')
+    okay(f.length, 3, 'array assignment arity')
+    okay(f(1, 2, 3), 6, 'array assignment')
     try {
         Operation([ object, 3 ])
     } catch (error) {
-        assert(error.message, 'expecting function or method name', 'function or method')
+        okay(error.message, 'expecting function or method name', 'function or method')
     }
     try {
         Operation([ 'method' ])
     } catch (error) {
-        assert(error.message, 'implicit object requires default object', 'default object')
+        okay(error.message, 'implicit object requires default object', 'default object')
     }
     try {
         Operation([ 3 ])
     } catch (error) {
-        assert(error.message, 'unable to determine desired operation', 'way wrong')
+        okay(error.message, 'unable to determine desired operation', 'way wrong')
     }
 }
